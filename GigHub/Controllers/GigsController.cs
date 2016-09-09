d0d4830654ca8c7a08eca2d3bc3afbd78ff9de8d
czajmen <kamil.czaja94@gmail.com>
2016-09-09 14:32:47 +0200

    Adding new Gig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -26,6 +28,36 @@ namespace GigHub.Controllers
             };
 
             return View(vm);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> Create(GigFormViewModel vm)
+        {
+
+
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genre.Single(g => g.Id == vm.Genre);
+
+
+
+
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", vm.Date, vm.Time)),
+                Genre = genre,
+                Venue = vm.Venue
+            };
+
+            _context.Gigs.Add(gig);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
