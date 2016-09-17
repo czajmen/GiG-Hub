00f7503c1193dfbffea24ae90184327bd3ada063
czajmen <kamil.czaja94@gmail.com>
2016-09-17 15:01:15 +0200

    Who I'm following page structure + VM + Controller

---
+++ GigHub/Controllers/FollowController.cs
@@ -0,0 +1,39 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class FollowController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FollowController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Follow
+        public ActionResult Follow()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var artists = _context.Followers
+                .Where(f => f.FollowerId == userId)
+                .Select(a => a.Followee)
+                .ToList();
+
+
+            var vm = new FollowingArtistsViewModel()
+            {
+                Artists = artists
+
+            };
+
+
+            return View(vm);
+        }
+    }
+}
---
+++ GigHub/ViewModels/FollowingArtistsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class FollowingArtistsViewModel
+    {
+        public IEnumerable<ApplicationUser> Artists { get; set; }
+
+    }
+}
